268adf8ded0bd981de3be550cc63d1d07b5a0e84
Yajaira Arauz <67719041+arauz-yajaira@users.noreply.github.com>
2023-08-31 14:10:31 -0600

    Se agregaron carpetas y archivos del proyecto

---
+++ SistemaEscaner/SistemaEscaner/USUARIOS/AgregarUsuario.Designer.cs
@@ -0,0 +1,231 @@
+﻿namespace SistemaEscaner.USUARIOS
+{
+    partial class AgregarUsuario
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.DGVUsuarios = new System.Windows.Forms.DataGridView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtUsuario = new System.Windows.Forms.TextBox();
+            this.txtContra = new System.Windows.Forms.TextBox();
+            this.cbTipoUsuario = new System.Windows.Forms.ComboBox();
+            this.cbEstado = new System.Windows.Forms.ComboBox();
+            this.BTNAgregar = new System.Windows.Forms.Button();
+            this.BTNModificar = new System.Windows.Forms.Button();
+            this.BTNContra = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.DGVUsuarios)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(439, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = " Usuarios";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.BTNContra);
+            this.panel1.Controls.Add(this.BTNModificar);
+            this.panel1.Controls.Add(this.BTNAgregar);
+            this.panel1.Controls.Add(this.cbEstado);
+            this.panel1.Controls.Add(this.cbTipoUsuario);
+            this.panel1.Controls.Add(this.txtContra);
+            this.panel1.Controls.Add(this.txtUsuario);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Location = new System.Drawing.Point(44, 98);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(352, 352);
+            this.panel1.TabIndex = 1;
+            // 
+            // DGVUsuarios
+            // 
+            this.DGVUsuarios.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DGVUsuarios.Location = new System.Drawing.Point(443, 87);
+            this.DGVUsuarios.Name = "DGVUsuarios";
+            this.DGVUsuarios.Size = new System.Drawing.Size(638, 394);
+            this.DGVUsuarios.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(100, 31);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(149, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Agregar Usuarios";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(58, 115);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(71, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Usuario";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(27, 158);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(102, 20);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Contraseña";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(40, 197);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(89, 15);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Tipo Usuario";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(78, 239);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(51, 15);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "Estado";
+            // 
+            // txtUsuario
+            // 
+            this.txtUsuario.Location = new System.Drawing.Point(135, 115);
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.Size = new System.Drawing.Size(168, 20);
+            this.txtUsuario.TabIndex = 8;
+            // 
+            // txtContra
+            // 
+            this.txtContra.Location = new System.Drawing.Point(135, 160);
+            this.txtContra.Name = "txtContra";
+            this.txtContra.Size = new System.Drawing.Size(168, 20);
+            this.txtContra.TabIndex = 9;
+            // 
+            // cbTipoUsuario
+            // 
+            this.cbTipoUsuario.FormattingEnabled = true;
+            this.cbTipoUsuario.Location = new System.Drawing.Point(135, 197);
+            this.cbTipoUsuario.Name = "cbTipoUsuario";
+            this.cbTipoUsuario.Size = new System.Drawing.Size(124, 21);
+            this.cbTipoUsuario.TabIndex = 10;
+            // 
+            // cbEstado
+            // 
+            this.cbEstado.FormattingEnabled = true;
+            this.cbEstado.Location = new System.Drawing.Point(135, 238);
+            this.cbEstado.Name = "cbEstado";
+            this.cbEstado.Size = new System.Drawing.Size(124, 21);
+            this.cbEstado.TabIndex = 11;
+            // 
+            // BTNAgregar
+            // 
+            this.BTNAgregar.Location = new System.Drawing.Point(43, 295);
+            this.BTNAgregar.Name = "BTNAgregar";
+            this.BTNAgregar.Size = new System.Drawing.Size(71, 35);
+            this.BTNAgregar.TabIndex = 12;
+            this.BTNAgregar.Text = "Agregar Usuario";
+            this.BTNAgregar.UseVisualStyleBackColor = true;
+            this.BTNAgregar.Click += new System.EventHandler(this.BTNAgregar_Click);
+            // 
+            // BTNModificar
+            // 
+            this.BTNModificar.Location = new System.Drawing.Point(146, 295);
+            this.BTNModificar.Name = "BTNModificar";
+            this.BTNModificar.Size = new System.Drawing.Size(71, 35);
+            this.BTNModificar.TabIndex = 13;
+            this.BTNModificar.Text = "Modificar Usuario";
+            this.BTNModificar.UseVisualStyleBackColor = true;
+            // 
+            // BTNContra
+            // 
+            this.BTNContra.Location = new System.Drawing.Point(253, 295);
+            this.BTNContra.Name = "BTNContra";
+            this.BTNContra.Size = new System.Drawing.Size(71, 35);
+            this.BTNContra.TabIndex = 14;
+            this.BTNContra.Text = "Cambiar Contraseña";
+            this.BTNContra.UseVisualStyleBackColor = true;
+            // 
+            // AgregarUsuario
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1125, 549);
+            this.Controls.Add(this.DGVUsuarios);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label1);
+            this.Name = "AgregarUsuario";
+            this.Text = "AgregarUsuario";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.DGVUsuarios)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.DataGridView DGVUsuarios;
+        private System.Windows.Forms.TextBox txtUsuario;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cbEstado;
+        private System.Windows.Forms.ComboBox cbTipoUsuario;
+        private System.Windows.Forms.TextBox txtContra;
+        private System.Windows.Forms.Button BTNContra;
+        private System.Windows.Forms.Button BTNModificar;
+        private System.Windows.Forms.Button BTNAgregar;
+    }
+}
