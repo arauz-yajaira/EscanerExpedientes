53dd883245356f5bae311598f524659e132a1c68
Yajaira Arauz <67719041+arauz-yajaira@users.noreply.github.com>
2023-09-19 11:24:34 -0600

    Creación de Form Expedientes
    Creación de formulario y un poco de diseño

---
+++ SistemaEscaner/SistemaEscaner/FORM/Expediente.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace SistemaEscaner.FORM
+{
+    partial class Expediente
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtBuscarExp = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.DGVDatos = new System.Windows.Forms.DataGridView();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.DGVDatos)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Lucida Sans", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(28, 112);
+            this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(142, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Buscar Paciente:";
+            // 
+            // txtBuscarExp
+            // 
+            this.txtBuscarExp.Location = new System.Drawing.Point(177, 109);
+            this.txtBuscarExp.Name = "txtBuscarExp";
+            this.txtBuscarExp.Size = new System.Drawing.Size(354, 26);
+            this.txtBuscarExp.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Lucida Sans", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(351, 24);
+            this.label4.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(298, 23);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "ARCHIVOS Y EXPEDIENTES";
+            // 
+            // DGVDatos
+            // 
+            this.DGVDatos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DGVDatos.Location = new System.Drawing.Point(80, 254);
+            this.DGVDatos.Name = "DGVDatos";
+            this.DGVDatos.Size = new System.Drawing.Size(809, 509);
+            this.DGVDatos.TabIndex = 7;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.PowderBlue;
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.txtBuscarExp);
+            this.panel1.Location = new System.Drawing.Point(81, 87);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(808, 161);
+            this.panel1.TabIndex = 8;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Lucida Sans", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(32, 48);
+            this.label2.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(659, 15);
+            this.label2.TabIndex = 4;
+            this.label2.Text = " Ingrese una de las opciones: Identidad, Nombre o Correlativo, para encontrar inf" +
+    "ormación de paciente.";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Lucida Sans", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(32, 33);
+            this.label3.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(107, 15);
+            this.label3.TabIndex = 5;
+            this.label3.Text = " Instrucciones:";
+            // 
+            // Expediente
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1035, 885);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.DGVDatos);
+            this.Controls.Add(this.label4);
+            this.Font = new System.Drawing.Font("Lucida Sans", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.Name = "Expediente";
+            this.Text = "Expediente";
+            ((System.ComponentModel.ISupportInitialize)(this.DGVDatos)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtBuscarExp;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DataGridView DGVDatos;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+    }
+}
---
+++ SistemaEscaner/SistemaEscaner/FORM/Expediente.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Drawing;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace SistemaEscaner.FORM
+{
+    public partial class Expediente : Form
+    {
+        //EntitiesEXP Entity = new EntitiesEXP();
+
+        public Expediente()
+        {
+            InitializeComponent();
+            CargarDGV();
+        }
+
+        private void CargarDGV()
+        {
+          
+        }
+
+    }
+}
